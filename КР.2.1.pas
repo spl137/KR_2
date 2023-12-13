4efb12b455d63cefce3641f277acf97c692503a1
spl137 <137485011+spl137@users.noreply.github.com>
2023-12-13 09:22:23 +0300

    Add files via upload

---
+++ КР.2.1.pas
@@ -0,0 +1,27 @@
+﻿var
+i,c:integer;
+a,n,s:array [1..10] of integer;
+begin
+c:=1;
+for i:=1 to 10 do begin
+readln(a[i]);
+if a[i] not in n then begin
+n[c]:=a[i];
+c:=c+1;
+end;
+end;
+for i:=1 to 10 do begin
+for c:=1 to 10 do begin
+if a[i]= n[c] then
+s[c]:=s[c]+1;
+end;
+end;  
+i:=1;
+for c:=2 to 10 do begin
+if (s[c] > s[c-1]) and (s[c]>0) then
+i:=c;
+if (s[c] < s[c-1]) and (s[c]>0) then
+i:=c-1;
+end;
+writeln('Наиболее часто встречающийся элемент: ', n[i]);
+end.
